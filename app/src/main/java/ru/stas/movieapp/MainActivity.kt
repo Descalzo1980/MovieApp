d3eecbba0408d1bb9475ce1bddf3e595bd6aca3b
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2023-01-23 17:51:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/stas/movieapp/MainActivity.kt
@@ -0,0 +1,27 @@
+package ru.stas.movieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import ru.stas.movieapp.navigation.SetupNavHost
+import ru.stas.movieapp.ui.theme.MovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MovieAppTheme {
+                val navController = rememberNavController()
+                SetupNavHost(navController = navController)
+            }
+        }
+    }
+}
